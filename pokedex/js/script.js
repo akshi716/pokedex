7e750bf197231d129e32a370712b518320150dd5
ENZO <112911179+akshi716@users.noreply.github.com>
2022-09-29 18:59:04 -0300

    Add files via upload

---
+++ pokedex/js/script.js
@@ -0,0 +1,32 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search')
+
+const fetchPokemon = async(pokemon) =>{
+
+const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+ const data = await APIResponse.json();
+return data;
+}
+const renderPokemon = async(pokemon) =>{
+
+    const data = await fetchPokemon (pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
+    ['animated']['front_default'];
+
+    console.log(data) 
+
+}
+
+
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+renderPokemon(input.value);
+input.value = '';
+
+});
